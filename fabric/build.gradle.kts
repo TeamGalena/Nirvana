735cc9c33551ff631b8e47786c0ce1ee9bce4419
Niklas Widmann <nik@somethingcatchy.net>
2025-09-06 10:44:48 +0200

    include multikulti core in fabric build

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -16,7 +16,7 @@ fabric {
 
     dependOn(project(":common"))
     includesMod("com.tterrag.registrate_fabric:Registrate:${registrate_fabric_version}")
-    // includesMod("com.possible-triangle:multikulti-core-fabric:${mc_version}-${multikulti_version}")
+    includesMod("com.possible-triangle:multikulti-core-fabric:${mc_version}-${multikulti_version}")
     includesMod("com.possible-triangle:multikulti-registrate-fabric:${mc_version}-${multikulti_version}")
     includesMod("dev.galena:hats-fabric:${mc_version}-${galena_hats_version}")
     includesMod("fuzs.forgeconfigapiport:forgeconfigapiport-fabric:${forge_config_port_version}")
