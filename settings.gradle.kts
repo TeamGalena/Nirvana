18d1b8306fc94de1e5a9a5659bfee2fa3279d326
Niklas Widmann <nik@somethingcatchy.net>
2025-10-07 13:27:25 +0200

    use published version of plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.possible-triangle.helper") version ("1.0.104")
+    id("com.possible-triangle.helper") version ("1.0.43")
 }
 
 include("common", "fabric", "forge")
