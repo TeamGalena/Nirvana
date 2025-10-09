90a0da946de7b59f480add852c0878f06e2c0bea
Niklas Widmann <nik@somethingcatchy.net>
2025-10-09 16:15:34 +0200

    catch skull model errors and print warning

--- common/src/main/java/galena/nirvana/mixins/SkullBlockRendererMixin.java
+++ common/src/main/java/galena/nirvana/mixins/SkullBlockRendererMixin.java
@@ -3,9 +3,12 @@ package galena.nirvana.mixins;
 import com.google.common.collect.ImmutableMap;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.authlib.GameProfile;
+import galena.nirvana.NirvanaConstants;
 import galena.nirvana.index.NirvanaBlocks;
 import galena.nirvana.world.block.renderer.ReeferHeadRenderer;
+
 import java.util.Map;
+
 import net.minecraft.client.model.SkullModel;
 import net.minecraft.client.model.SkullModelBase;
 import net.minecraft.client.model.geom.EntityModelSet;
@@ -36,7 +39,11 @@ public class SkullBlockRendererMixin {
             at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMap$Builder;build()Lcom/google/common/collect/ImmutableMap;")
     )
     private static void registerReeferModel(EntityModelSet set, CallbackInfoReturnable<Map<SkullBlock.Type, SkullModelBase>> cir, @Local ImmutableMap.Builder<SkullBlock.Type, SkullModelBase> builder) {
-        builder.put(NirvanaBlocks.REEFER_SKULL_TYPE, new SkullModel(set.bakeLayer(ReeferHeadRenderer.LAYER)));
+        try {
+            builder.put(NirvanaBlocks.REEFER_SKULL_TYPE, new SkullModel(set.bakeLayer(ReeferHeadRenderer.LAYER)));
+        } catch (IllegalArgumentException ex) {
+            NirvanaConstants.LOGGER.warn("unable to create reefer skull model. This can happen if another mod already breaks before nirvana. If nothing else fails, please report this");
+        }
     }
 
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.possible-triangle.helper") version ("1.0.43")
+    id("com.possible-triangle.helper") version ("1.0.48")
 }
 
 include("common", "fabric", "forge")
